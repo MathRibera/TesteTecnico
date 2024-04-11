958d70ce8d8c573de3ab97dc534b017c8aa9d7b8
Matheus Jorge <bs.fenixfl@gmail.com>
2024-04-11 00:13:07 -0300

    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body className={`${inter.className} bg-zinc-100`}>{children}</body>
     </html>
   );
