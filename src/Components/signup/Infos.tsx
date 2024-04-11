a59d79e68ac7958dca45636de9cc3141abd36d97
Matheus Jorge <bs.fenixfl@gmail.com>
2024-04-10 23:23:29 -0300

    feat: Criada a pagina de registro do usuario

---
+++ src/Components/signup/Infos.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function Infos() {
+  return (
+    <div className="self-stretch flex flex-col items-center justify-center gap-[16px] max-w-full text-left text-sm text-text-text-muted-foreground font-inter">
+      <input
+        className="w-full [border:none] [outline:none] bg-background-bg-background self-stretch h-10 rounded-radius-rounded-md overflow-hidden shrink-0 flex flex-row items-center justify-start py-2.5 px-3 box-border font-inter text-sm text-text-text-muted-foreground min-w-[250px]"
+        placeholder="Nome"
+        type="text"
+      /><input
+      className="w-full [border:none] [outline:none] bg-background-bg-background self-stretch h-10 rounded-radius-rounded-md overflow-hidden shrink-0 flex flex-row items-center justify-start py-2.5 px-3 box-border font-inter text-sm text-text-text-muted-foreground min-w-[250px]"
+      placeholder="Sobrenome"
+      type="text"
+    />
+      <input
+        className="w-full [border:none] [outline:none] bg-background-bg-background self-stretch h-10 rounded-radius-rounded-md overflow-hidden shrink-0 flex flex-row items-center justify-start py-2.5 px-3 box-border font-inter text-sm text-text-text-muted-foreground min-w-[250px]"
+        placeholder="E-mail"
+        type="text"
+      />
+      <input
+        className="w-full [border:none] [outline:none] bg-background-bg-background self-stretch h-10 rounded-radius-rounded-md overflow-hidden shrink-0 flex flex-row items-center justify-start py-2.5 px-3 box-border font-inter text-sm text-text-text-muted-foreground min-w-[250px]"
+        placeholder="Senha"
+        type="text"
+      />
+      <input
+        className="w-full [border:none] [outline:none] bg-background-bg-background self-stretch h-10 rounded-radius-rounded-md overflow-hidden shrink-0 flex flex-row items-center justify-start py-2.5 px-3 box-border font-inter text-sm text-text-text-muted-foreground min-w-[250px]"
+        placeholder="Confirme a senha"
+        type="text"
+      />
+      <div className="self-stretch flex flex-row items-end justify-start py-0 px-[5px] box-border max-w-full text-lightslategray">
+        <div className="flex-1 relative inline-block max-w-full">
+          <span>{`Ao criar uma conta, você concorda com nossa `}</span>
+          <b>Política de Privacidade</b>
+          <span>{` e `}</span>
+          <b>Política de Comunicação Eletrônica</b>
+        </div>
+      </div>
+      <button className="cursor-pointer [border:none] py-2.5 px-5 bg-main-orange self-stretch rounded-radius-rounded-md flex flex-row items-center justify-center gap-[8px]">
+        <img
+          className="h-4 w-4 relative overflow-hidden shrink-0 hidden"
+          alt=""
+          src="/badgecheck.svg"
+        />
+        <b className="relative text-sm leading-[20px] inline-block font-inter text-text-text-primary-foreground text-left min-w-[89px]">
+          CADASTRAR
+        </b>
+        <img
+          className="h-4 w-4 relative overflow-hidden shrink-0 hidden"
+          alt=""
+          src="/badgecheck.svg"
+        />
+      </button>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,35 @@
+import Social from '@/Components/Login/Social';
+import Infos from '@/Components/signup/Infos';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className='w-[full] max-w-full overflow-hidden flex flex-col items-center justify-start pt-[199.5px] pb-[215.5px] pr-[21px] pl-5 box-border gap-[40px] leading-[normal] tracking-[normal] text-center text-[60px] text-black font-inter mq450:gap-[35px] mq675:gap-[70px]'>
+      <h1 className='m-0 w-[491px] text-4xl relative text-inherit font-inherit inline-block font-bold'>
+        Sign up
+      </h1>
+      <div className='w-[491px] hidden max-w-full' />
+      <div className='w-[490px] flex flex-col items-start justify-start gap-[32px] max-w-full text-left text-[16px] mq675:gap-[16px]'>
+        <form className='m-0 self-stretch flex flex-col items-center justify-center gap-[16px] max-w-full'>
+
+          <Infos />
+        </form>
+        <Social text='Registrar' />
+        <div className='self-stretch flex flex-col items-start justify-start gap-[24px] max-w-full'>
+          <div className='self-stretch h-0 flex flex-row items-start justify-start pt-0 px-0 pb-0 box-border max-w-full'>
+            <div className='h-px flex-1 relative box-border max-w-full border-t-[1px] border-solid border-aliceblue' />
+          </div>
+          <div className='self-stretch flex flex-row items-start justify-center py-0 pr-[21px] pl-5'>
+            <div className='relative leading-[16px]'>
+              <span>Já tem uma conta?</span>
+              <Link href='/'>
+                {' '}
+                <strong>Entrar</strong>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
